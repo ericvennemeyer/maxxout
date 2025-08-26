726da8d7d92e5e38999addc48a9f5a6cbdb41fa1
Eric Vennemeyer <henri.barun@gmail.com>
2025-08-26 12:09:12 -0700

    Create function to animate in blocks in series

--- level.gd
+++ level.gd
@@ -4,12 +4,30 @@ extends Node2D
 signal level_complete
 
 var block_array: Array
+var animation_duration: float = 1.0
+var animation_delay: float = 0.0
 
 
 func _ready() -> void:
 	block_array = get_children()
 	for block in block_array:
 		block.destroyed.connect(_on_block_destroyed)
+		block.scale = Vector2.ZERO
+		block.modulate = Color(1.0, 1.0, 1.0, 0.0)
+	
+	animate_in()
+
+
+func animate_in() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_parallel()
+	
+	for block in block_array:
+		tween.tween_property(block, "scale", Vector2(1.0, 1.0), animation_duration).set_delay(animation_delay)
+		tween.tween_property(block, "modulate", Color(1.0, 1.0, 1.0, 1.0), animation_duration * 2).set_delay(animation_delay)
+		animation_delay += 0.05
 
 
 func _on_block_destroyed(block: Block) -> void:
