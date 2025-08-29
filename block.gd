c97a21e694f9a73ccdcf8716ce0700ef22106e4d
Eric Vennemeyer <henri.barun@gmail.com>
2025-08-29 15:04:12 -0700

    Add bigger destruction particles for blocks

--- block.gd
+++ block.gd
@@ -8,6 +8,7 @@ enum BlockColor {RED, GREEN, BLUE, YELLOW}
 @export var block_color: BlockColor
 
 @export var block_particles: PackedScene
+@export var destruction_particles: PackedScene
 
 var color_data: Color
 var current_node_parent: Node
@@ -38,20 +39,21 @@ func destroy() -> void:
 	var tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_SPRING)
 	tween.tween_property(self, "scale", scale * 1.2, 0.2)
-	instantiate_particles()
 	
 	block_color += 1
 	if block_color >= BlockColor.size():
+		instantiate_particles(destruction_particles)
 		Global.play_sfx("block_destroy")
 		destroyed.emit(self)
 		tween.tween_callback(queue_free)
 	else:
+		instantiate_particles(block_particles)
 		set_block_color()
 		tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
 
 
-func instantiate_particles() -> void:
-	var new_particles = block_particles.instantiate()
+func instantiate_particles(particles: PackedScene) -> void:
+	var new_particles = particles.instantiate()
 	new_particles.position = position
 	new_particles.color = color_data
 	new_particles.finished.connect(func(): new_particles.queue_free())
